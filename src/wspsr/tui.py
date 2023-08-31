c18998555c5ea923189a707ec9577dcf57b7010c
Jan Inge Sande <j@ninge.no>
2023-08-31 14:22:30 +0200

    Renamed main module to tui

--- src/wspsr/tui.py
+++ src/wspsr/tui.py
@@ -18,11 +18,20 @@ class WSPSRApp(App):
     tasks: dict[str, dict] = {}
     defaults = {'models': ['large-v2', 'diarize']}
 
+    def __init__(self, driver_class=None, css_path=None, watch_css=None, monitor_path=None):
+        super().__init__(driver_class, css_path, watch_css)
+        self.monitor_path = monitor_path
+
     def on_mount(self) -> None:
-        self.push_screen(SelectionScreen("/media"))
+        self.push_screen(SelectionScreen(self.monitor_path))
         # self.push_screen(SetEncryptionKeyScreen())
 
 
-if __name__ == "__main__":
-    app = WSPSRApp()
+def main():
+    from sys import argv
+    app = WSPSRApp(monitor_path="/media" if len(argv) <= 1 else argv[1])
     app.run()
+
+
+if __name__ == "__main__":
+    main()
